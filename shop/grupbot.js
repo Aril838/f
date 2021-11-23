0b4f16a8092ce024ecc40877e2a86021df4c3739
Aril838 <94908745+Aril838@users.noreply.github.com>
2021-11-23 20:17:23 +0700

    grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,14 +2,14 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+1. *Aril Botz Support*
+_https://chat.whatsapp.com/E27JNkwARwBLu6ELvWclDr_
+2. *Aril Botz Support 2*
+_https://chat.whatsapp.com/D7JJ5nWcjOZ3MsCJ0hmws4_
+3. *Aril Botz Support 3*
+_https://chat.whatsapp.com/BrgTOt16AOACNrCzRsaNJy_
+4. *Aril Botz Support 4* 
+_https://chat.whatsapp.com/HFjt80QKJRnHN8ni1oHt1y_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
